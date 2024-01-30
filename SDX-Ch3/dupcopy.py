54b474b6df768749f63086f8f9089bb07bcf8f33
TMahlatini <mahlatit@whitman.edu>
2024-01-30 14:19:29 -0800

    added a new function to deal with exercise part 2

--- SDX-Ch3/dupcopy.py
+++ SDX-Ch3/dupcopy.py
@@ -11,16 +11,18 @@ def find_groups(filename):
         groups[hash_code].add(fn)
     return groups
 
-def find_similar_file(filename, file='known_files.txt'):
-    groups = {}
-    for fn in filename:
-        data = open(fn, "rb").read()
-        hash_code = sha256(data).hexdigest()
-        # if hash code in known_files
-        if hash_code not in groups:
-            groups[hash_code] = set()
-        groups[hash_code].add(fn)
-    return groups
+def add_new_sound(filename, file='known_files.txt'):
+    data = open(filename, "rb").read()
+    hash_code = sha256(data).hexdigest()
+    duplicate = False 
+    for code in file.readlines():
+        if hash_code == code: 
+            duplicate = True
+    if not duplicate:
+        file = open(file, "a")
+        file.write(f"{hash_code}\n")
+
+    return file 
 
 
 if __name__ == "__main__":
