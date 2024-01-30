4d98c8e67a6994d27d0c6763081fdc90b368a1e3
Chris Gomez <97072661+gomezc08@users.noreply.github.com>
2024-01-30 13:21:45 -0800

    Update brute_force_1.py

--- SDX-Ch3/brute_force_1.py
+++ SDX-Ch3/brute_force_1.py
@@ -1,22 +1,21 @@
 import sys
 
-
+# [main]
+def find_duplicates(filenames):
+    matches = []
+    for left in filenames:
+        for right in filenames:
+            # added memo aspect.
+            if same_bytes(left, right) and [filenames[i], filenames[j]] not in matches:
+                matches.append((left, right))
+    return matches
+    
 # [bytes]
 def same_bytes(left_name, right_name):
     left_bytes = open(left_name, "rb").read()
     right_bytes = open(right_name, "rb").read()
     return left_bytes == right_bytes
 # [/bytes]
-
-
-# [main]
-def find_duplicates(filenames):
-    matches = []
-    for left in filenames:
-        for right in filenames:
-            if same_bytes(left, right):
-                matches.append((left, right))
-    return matches
 
 
 if __name__ == "__main__":
