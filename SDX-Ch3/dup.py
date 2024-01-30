0673a752b1d7ef438bcc8bbb4fdb7aab483c0a01
gabepm100 <85536768+gabepm100@users.noreply.github.com>
2024-01-30 13:26:14 -0800

    Update dup.py

--- SDX-Ch3/dup.py
+++ SDX-Ch3/dup.py
@@ -2,13 +2,20 @@ import sys
 from hashlib import sha256
 
 def find_groups(filenames):
+    creates a dictionary groups
     groups = {}
+    #for each file in filenames
     for fn in filenames:
+        #open files
         data = open(fn, "rb").read()
+        #creates a string for the file
         hash_code = sha256(data).hexdigest()
         if hash_code not in groups:
+            #if the string is not in groups then create a set as the value and the hash code as the key
             groups[hash_code] = set()
+        #if a set is already created or just created add the file as the value and the hash code as the key.
         groups[hash_code].add(fn)
+        
     return groups
 
 
