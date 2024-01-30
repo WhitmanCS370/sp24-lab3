7cfc46926ea4a48f1a9691db3527a4ded49e1b4a
gabepm100 <85536768+gabepm100@users.noreply.github.com>
2024-01-30 13:20:29 -0800

    Update brute_force_1.py

--- SDX-Ch3/brute_force_1.py
+++ SDX-Ch3/brute_force_1.py
@@ -3,6 +3,7 @@ import sys
 
 # [bytes]
 def same_bytes(left_name, right_name):
+    #opens the left and right files and returns whether or not the bites are the same
     left_bytes = open(left_name, "rb").read()
     right_bytes = open(right_name, "rb").read()
     return left_bytes == right_bytes
@@ -12,9 +13,12 @@ def same_bytes(left_name, right_name):
 # [main]
 def find_duplicates(filenames):
     matches = []
+    #for every character in the left file it goes through every character in the right file.
     for left in filenames:
         for right in filenames:
+            #for each of these pairs of characters it calls the same bytes function
             if same_bytes(left, right):
+                #if they are the same then append the match to the list matches
                 matches.append((left, right))
     return matches
 
